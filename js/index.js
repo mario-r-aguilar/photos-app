76373d20345dfa0f38356748a209a043bae3fab3
Mario Aguilar <mariocba150@gmail.com>
2024-07-22 23:40:42 -0300

    inicio repositorio

---
+++ js/index.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const video = document.getElementById('video');
+	const snap = document.getElementById('snap');
+	const canvas = document.getElementById('canvas');
+	const context = canvas.getContext('2d');
+	const photo = document.getElementById('photo');
+	//const gallery = document.getElementById('gallery');
+
+	// Solicitud de acceso a la cámara
+	navigator.mediaDevices
+		.getUserMedia({ video: true })
+		.then((stream) => {
+			video.srcObject = stream;
+		})
+		.catch((error) => {
+			console.error('Error al intentar acceder a la cámara', error);
+		});
+
+	// Tomar foto
+	snap.addEventListener('click', () => {
+		canvas.width = video.videoWidth;
+		canvas.height = video.videoHeight;
+		context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+		// Convertir canvas a URL
+		const dataURL = canvas.toDataURL('image/png');
+		photo.setAttribute('src', dataURL);
+		photo.style.display = 'block';
+
+		// Llamar a función para guardar foto en el localStorage
+		savePhoto(dataURL);
+		displayPhotos();
+	});
+
+	// Guardar foto en el localStorage
+	function savePhoto(dataURL) {
+		let photos = JSON.parse(localStorage.getItem('photos')) || [];
+		photos.push(dataURL);
+		localStorage.setItem('photos', JSON.stringify(photos));
+	}
+
+	// Mostrar fotos en la galería
+	function displayPhotos() {
+		let photos = JSON.parse(localStorage.getItem('photos')) || [];
+		gallery.innerHTML = '';
+		if (photos.length === 0) {
+			gallery.innerHTML = '<p>No hay fotos en la galería.</p>';
+		} else {
+			photos.forEach((photoURL, index) => {
+				let imgContainer = document.createElement('div');
+				imgContainer.classList.add('gallery-item');
+
+				let img = document.createElement('img');
+				img.src = photoURL;
+				img.classList.add('gallery-photo');
+				imgContainer.appendChild(img);
+
+				let deleteButton = document.createElement('button');
+				deleteButton.textContent = 'Eliminar';
+				deleteButton.classList.add('delete-button');
+				deleteButton.addEventListener('click', () => {
+					deletePhoto(index);
+				});
+
+				imgContainer.appendChild(deleteButton);
+				gallery.appendChild(imgContainer);
+			});
+		}
+	}
+
+	// Eliminar foto
+	function deletePhoto(index) {
+		let photos = JSON.parse(localStorage.getItem('photos')) || [];
+		photos.splice(index, 1);
+		localStorage.setItem('photos', JSON.stringify(photos));
+		displayPhotos();
+	}
+
+	// Mostrar galería al cargar la página
+	displayPhotos();
+});
